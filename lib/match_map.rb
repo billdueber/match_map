6aaace883624cdb9d436f0bb6dc1851f82cf8f4e
Bill Dueber <bill@dueber.com>
2011-10-23 21:08:07 -0400

    Removing dead code

--- lib/match_map.rb
+++ lib/match_map.rb
@@ -1,10 +1,14 @@
+# A hash-like object that tries to match an argument against
+# *all* keys (using == for non-Regexp keys and pattern matching 
+# for Regexp keys)
+
 class MatchMap
-  attr_accessor :pchecks, :default
-  attr_reader :echo
+  
+  attr_accessor :default
+  attr_reader   :echo
   
   def initialize echo = nil
     @default = nil # default miss value is nil
-    @pchecks = 0
     @echo = echo
     @keys = []
     @map = {}
@@ -43,10 +47,6 @@ class MatchMap
     
   
   def inner_get arg
-    # unless @dirty
-    #   @pchecks += 1
-    #   return self.default unless @super_regexp.match arg
-    # end
     rv = []
     @keys.each do |k|
       if k.is_a? Regexp
@@ -77,41 +77,40 @@ class MatchMap
   end
         
   
-  # Do what we can to reduce the number of regexp calls
-  # It turns out they're expensive, so if you're going to
-  # be using a map a few times, it's worth the effort.
-  # 
-  def optimize
-    return unless @dirty
-    # Make a super-regexp that matches everything
-    @super_regexp = Regexp.union self.keys
-    
-    # Build an inverted set for cases where a single value or set of
-    # values map to multiple patterns
-    #
-    # First, build up the inverted map
-    inverted = {}
-    self.each_pair do |p,v|
-      inverted[v] ||= []
-      inverted[v] << p
-    end
-    # Now, find places where values are repeated and just build
-    # a single larger regexp for it. e.g., if 
-    # /a/ => 1 and /b/ => 1, then replace them both with
-    # /a|b/ => 1. 
-    inverted.each_pair do |vals, patterns|
-      next unless patterns.size > 1
-      newpat = Regexp.union patterns
-      if self.has_key? newpat
-        vals += self.old_get newpat
-        vals.uniq!
-      end
-      patterns.each do |p|
-        self.delete p
-      end
-      self[newpat] = vals
-    end
-    
-    @dirty = false
-  end
+  # # Do what we can to reduce the number of regexp calls
+  # # It turns out they're expensive, so if you're going to
+  # # be using a map a few times, it's worth the effort.
+  # # 
+  # def optimize
+  #   return unless @dirty
+  #   # Make a super-regexp that matches everything
+  #   @super_regexp = Regexp.union self.keys
+  #   
+  #   # Build an inverted set for cases where a single value or set of
+  #   # values map to multiple patterns
+  #   #
+  #   # First, build up the inverted map
+  #   inverted = {}
+  #   self.each_pair do |p,v|
+  #     inverted[v] ||= []
+  #     inverted[v] << p
+  #   end
+  #   # Now, find places where values are repeated and just build
+  #   # a single larger regexp for it. e.g., if 
+  #   # /a/ => 1 and /b/ => 1, then replace them both with
+  #   # /a|b/ => 1. 
+  #   inverted.each_pair do |vals, patterns|
+  #     next unless patterns.size > 1
+  #     newpat = Regexp.union patterns
+  #     if self.has_key? newpat
+  #       vals += self.old_get newpat
+  #       vals.uniq!
+  #     end
+  #     patterns.each do |p|
+  #       self.delete p
+  #     end
+  #     self[newpat] = vals
+  #   end
+  #   
+  # end
 end

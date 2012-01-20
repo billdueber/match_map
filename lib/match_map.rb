538f25e0dde9b91d642b40c45806670d18a1ad72
BillDueber <bill@dueber.com>
2012-01-20 09:26:15 -0500

    Remove deprecated optimize method

--- lib/match_map.rb
+++ lib/match_map.rb
@@ -76,41 +76,4 @@ class MatchMap
     @keys.delete(key)
   end
 
-
-  # # Do what we can to reduce the number of regexp calls
-  # # It turns out they're expensive, so if you're going to
-  # # be using a map a few times, it's worth the effort.
-  # #
-  # def optimize
-  #   return unless @dirty
-  #   # Make a super-regexp that matches everything
-  #   @super_regexp = Regexp.union self.keys
-  #
-  #   # Build an inverted set for cases where a single value or set of
-  #   # values map to multiple patterns
-  #   #
-  #   # First, build up the inverted map
-  #   inverted = {}
-  #   self.each_pair do |p,v|
-  #     inverted[v] ||= []
-  #     inverted[v] << p
-  #   end
-  #   # Now, find places where values are repeated and just build
-  #   # a single larger regexp for it. e.g., if
-  #   # /a/ => 1 and /b/ => 1, then replace them both with
-  #   # /a|b/ => 1.
-  #   inverted.each_pair do |vals, patterns|
-  #     next unless patterns.size > 1
-  #     newpat = Regexp.union patterns
-  #     if self.has_key? newpat
-  #       vals += self.old_get newpat
-  #       vals.uniq!
-  #     end
-  #     patterns.each do |p|
-  #       self.delete p
-  #     end
-  #     self[newpat] = vals
-  #   end
-  #
-  # end
 end
